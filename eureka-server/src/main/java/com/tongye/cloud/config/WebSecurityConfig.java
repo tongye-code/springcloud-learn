5b36102ce213fd12c920639cb1850cbf18f31467
tongye <1615715823@qq.com>
2021-12-01 09:40:42 +0800

    eureka服务注册与发现

---
+++ eureka-server/src/main/java/com/tongye/cloud/config/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.tongye.cloud.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * <p>
+ *
+ * <p>
+ *
+ * @Author zhuhanqing
+ * @Date 2021/11/30 18:09
+ * @Version 1.0.0
+ */
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().ignoringAntMatchers("/eureka/**");
+        super.configure(http);
+    }
+}
